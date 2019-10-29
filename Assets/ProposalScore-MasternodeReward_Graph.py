b233b4864aaa4f3e53a65e1fca630c7bdaefd48a
user <17202046+hibpm@users.noreply.github.com>
2019-10-29 23:12:16 +0300

    Initial commit

---
+++ Assets/ProposalScore-MasternodeReward_Graph.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+k = 1000
+m = 500
+n = m + k
+
+proposalScore = [0, 0.5, 1]
+masternodeRewardYes = [0, 1, n/k]
+masternodeRewardNo = [n/m, 1, 0]
+
+plt.plot(proposalScore, masternodeRewardYes,
+         color='#6aa84fff', label='Masternode voted «Yes»')
+plt.plot(proposalScore, masternodeRewardNo,
+         color='#db4437ff', label='Masternode voted «No»')
+
+plt.xlabel('ProposalScore')
+plt.ylabel('MasternodeReward')
+plt.title('Example for k=%d, m=%d' % (k, m))
+plt.legend()
+
+plt.savefig('ProposalScore-MasternodeReward_Graph.svg')
